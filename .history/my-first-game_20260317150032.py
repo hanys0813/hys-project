d9d188942d30c2fec8a679777c96227306f4ab72
hys0813 <20260199@vision.hoseo.edu>
2026-04-17 14:57:12 +0900

    기획안 변경

--- .history/my-first-game_20260317150032.py
+++
@@ -1,9 +0,0 @@
-import pygame
-import sys
-pygame.init()
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("My First Pygame")
-WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
-clock = pygame.time.Clock()
-running = True
